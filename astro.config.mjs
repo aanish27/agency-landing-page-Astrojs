5cdf18a5c957eefef0dd07c3aedcdfa23c20e5be
aanish27 <aanish2710@gmail.com>
2024-12-12 16:18:47 -0500

    deploye

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
+  site: 'https://aanish27.astro.github.io',
+  base: '/agency-landing-page-Astrojs',
 });
+
