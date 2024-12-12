2fd9d28e548b0c422d4f0a718ee4dceae0225e75
aanish27 <aanish2710@gmail.com>
2024-12-12 16:20:36 -0500

    err

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
   site: 'https://aanish27.astro.github.io',
   base: '/agency-landing-page-Astrojs',
 });
